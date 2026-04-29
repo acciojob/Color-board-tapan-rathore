ab5ee7eb0e9e15779f9caebbfbb70c39f3084c51
Tapan Rathore <tapanrathore07@gmail.com>
2026-04-29 22:46:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+let container = document.querySelector(".container")
+let fragment = document.createDocumentFragment();
+for(let i=0; i<800; i++){
+	let square = document.createElement("div");
+	square.classList.add("square");
+	fragment.append(square)
+}
+container.append(fragment)
+
+let squares = document.querySelectorAll(".square")
+
+function getRandomColor(){
+	return `rgb(${Math.floor(Math.random()*255)},
+				${Math.floor(Math.random()*255)},
+				${Math.floor(Math.random()*255)})`;
+}
+
+for(let square of squares){
+	square.addEventListener("mouseenter", ()=>{
+		square.style.backgroundColor=getRandomColor();
+		
+		setTimeout(()=>{
+			square.style.backgroundColor = "#111"
+		}, 1000)
+
+		
+	});
+
+}
+
